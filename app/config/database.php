8719dcc8a6626b1ccf8ba1f4c9bd14a051d43dd6
ArguellesNeil <neilskiey19@gmail.com>
2025-09-19 11:54:53 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -63,7 +63,7 @@ $database['main'] = array(
     'username' => getenv('DB_USER'),
     'password' => getenv('DB_PASS'),
     'database' => getenv('DB_NAME'),
-    'port'     => getenv('DB_POST'),
+    'port'     => getenv('DB_PORT'),
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
